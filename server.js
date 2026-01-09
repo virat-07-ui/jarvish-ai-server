ddb5637611b5aaf34b4d393d0d69c647a97aff17
Kaushal <kaushalchaudhary.10101@gmail.com>
2026-01-09 10:38:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.post("/ai", async (req, res) => {
     const userText = req.body.text;
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" +
+      "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=" +
         process.env.GEMINI_API_KEY,
       {
         method: "POST",
